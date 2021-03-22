a0ac145b2077434144549d99f37a555a5347f9aa
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-22 23:42:03 +0500

    new react app

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Dinners from "./dinner";
 
 function App() {
   return (
     <div className="App">
+      <Dinners/>
       <h1>My Favourite 5 Movies</h1>
     </div>
   );
---
+++ demo/src/dinner.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Dinners(){
+    return <h1>I had chicken Biryani at DInner</h1>
+};
+export default Dinners;
