dbcb145f9b735a9a744fa9175667fd801664ab9d
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 02:42:14 +0500

    Update App.js

--- practice/src/App.js
+++ practice/src/App.js
@@ -4,11 +4,11 @@ import Card from "./card";
 function App() {
   return (
     <div className="App">
-      <Card imgsrc="/images/CI1.jpg" SName="Sacred Games" link="https://www.netflix.com/pk/title/80115328"/>
-      <Card imgsrc="/images/CI2.jpg" SName="Mirzapur" link="https://www.amazon.com/Mirzapur-Season-1/dp/B086VXR733"/>
-      <Card imgsrc="/images/CI3.jpg" SName="Money Heist" link="https://www.netflix.com/pk/title/80192098"/>
-      <Card imgsrc="/images/CI4.jpg" SName="Dark" link="https://www.netflix.com/pk/title/80100172"/>
-      <Card imgsrc="/images/CI5.jpg" SName="Ertugrul" link=""/>
+      <Card imgsrc="/images/CI1.jpg" title="A Netflix Original Series" SName="Sacred Games" link="https://www.netflix.com/pk/title/80115328"/>
+      <Card imgsrc="/images/CI2.jpg" title="An Amazon Prime Series" SName="Mirzapur" link="https://www.amazon.com/Mirzapur-Season-1/dp/B086VXR733"/>
+      <Card imgsrc="/images/CI3.jpg" title="A Netflix Original Series" SName="Money Heist" link="https://www.netflix.com/pk/title/80192098"/>
+      <Card imgsrc="/images/CI4.jpg" title="A Netflix Original Series" SName="Dark" link="https://www.netflix.com/pk/title/80100172"/>
+      <Card imgsrc="/images/CI5.jpg" title="A Netflix Original Series" SName="Ertugrul" link="https://www.netflix.com/pk/title/80127001"/>
     </div>
   );
 }
