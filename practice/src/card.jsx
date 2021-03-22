cd606d1255c2493499ee12dbde87254c1405ab52
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-22 23:54:45 +0500

    making card

---
+++ practice/src/card.jsx
@@ -0,0 +1,22 @@
+function card(){
+    return(
+        <div className="cards">
+            <div className="card">
+                <img src="" alt=" " className="card_img"/>
+                <div className="card_info">
+                    <span className="card_catagory">
+                        Netflix Original Series
+                    </span>
+                    <h3 className="card_title">
+
+                    </h3>
+                    <a href="" target="_blank">
+                        <button>Watch Now</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default card
