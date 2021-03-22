7142b1ef2ec909cfdf4f37357fca8de0d4693379
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 02:17:32 +0500

    Update App.js

--- practice/src/App.js
+++ practice/src/App.js
@@ -4,6 +4,10 @@ import Card from "./card";
 function App() {
   return (
     <div className="App">
+      <Card imgsrc="" SName="" link=""/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Card/>
     </div>
   );
