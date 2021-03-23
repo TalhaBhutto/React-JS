20106b49adbff66d853b3d4e19b0ecdc3b27194a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 17:00:45 +0500

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Dinners from "./dinner";
 
+const newdata= Education=[
+  {I_name:"DASKBZ" ,Passing_Year:2015 , degree:"Matric Science" },
+  {I_name:"NCR-CET" ,Passing_Year:2017 , degree:"Inter Science" },
+  {I_name:"UoK" ,Passing_Year:2022 , degree:"BS-Software Engineering" },
+]
 function App() {
   return (
     <div className="App">
