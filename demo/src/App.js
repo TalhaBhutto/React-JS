d88db32e7fd2dbc265e72da11ca86371f54f770f
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 17:06:06 +0500

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Dinners from "./dinner";
 
-const newdata= Education=[
+const Education=[
   {I_name:"DASKBZ" ,Passing_Year:2015 , degree:"Matric Science" },
   {I_name:"NCR-CET" ,Passing_Year:2017 , degree:"Inter Science" },
-  {I_name:"UoK" ,Passing_Year:2022 , degree:"BS-Software Engineering" },
+  {I_name:"UoK" ,Passing_Year:2022 , degree:"BS-Software Engineering" }
 ]
+const data=Education.map((cval)=>{
+return `I have done my ${cval.degree} from ${cval.I_name} in ${cval.Passing_Year}`
+});
+console.log(data);
 function App() {
   return (
     <div className="App">
