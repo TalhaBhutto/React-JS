a5f0816d5e1e602c8ceb4b3a3ef7b362b710965c
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-25 17:58:42 +0500

    applying css

--- practice2/src/App.js
+++ practice2/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 const App = ()=>{
 
-  return(<div className="MainDiv">
-
-  </div>);
+  return(
+  <div className="MainDiv">
+    <div className="centerDiv">
+      <br/>
+      <h1>ToDO List</h1>
+      <br/>
+      <input type="text" placeholder="Add Item"/>
+      <button> + </button>
+      <ol>
+        <li>buy Apple</li>
+      </ol>
+    </div>
+  </div>
+  );
 };
 export default App;
