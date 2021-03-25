7e9c7d8a9ba8e24b595fd05b29af150e9b379d32
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-25 18:27:15 +0500

    styling done. Hooks start

--- practice2/src/App.js
+++ practice2/src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 const App = ()=>{
-
+  const [inputList,SetInputList]=useState("");
+  const itemEvents=(event)=>{
+    SetInputList(event.target.value);
+  }
   return(
   <div className="MainDiv">
     <div className="centerDiv">
       <br/>
       <h1>ToDo List</h1>
       <br/>
-      <input type="text" placeholder="Add Item"/>
+      <input type="text" placeholder="Add Item" onChange={itemEvents}/>
       <button> + </button>
       <ol>
-        <li>buy Apple</li>
+        <li>{inputList}</li>
       </ol>
     </div>
   </div>
