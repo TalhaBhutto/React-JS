c1881841813ba1dbe1a71a1bc68bb65e8cc9e127
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-26 19:20:36 +0500

    ExpenceTrackerDone

--- practice2/src/App.js
+++ practice2/src/App.js
@@ -2,30 +2,43 @@ import React, { useState } from "react";
 import List from "./list";
 const App = ()=>{
   const [inputList,SetInputList]=useState("");
+  const [inputPrice,SetInputPrice]=useState("");
   const [items,SetItems]=useState([]);
   const itemEvents=(event)=>{
     SetInputList(event.target.value);
   }
+  const [expence,Setexpence]=useState(0);
+  let Exp=0;
+  const PriceEvents=(event)=>{
+    SetInputPrice(event.target.value);
+  }
   const ListOfItems=()=>{
     SetItems((OItems)=>{
-      return [...OItems,inputList]
+      return [...OItems,{item:inputList+" : ",price:inputPrice}]
     });
     SetInputList("");
+    SetInputPrice("");
+    Exp=parseInt(expence)+parseInt(inputPrice);
+    Setexpence(Exp);
   }
-  const DeleteItems=(id)=>{
+  const DeleteItems=(id,price)=>{
       SetItems((OldItems)=>{
         return OldItems.filter((arrElem,index)=>{
           return index != id;
         });
-      });  
+      });
+      Exp=parseInt(expence)-parseInt(price);
+      Setexpence(Exp);  
   };
   return(
   <div className="MainDiv">
     <div className="centerDiv">
       <br/>
-      <h1>ToDo List</h1>
+      <h1 className="heading">Expence Teacker</h1>
       <br/>
+      <h1 className="result">Total : {expence} RS</h1>
       <input type="text" placeholder="Add Item" onChange={itemEvents} value={inputList} />
+      <input type="number" placeholder="Add Price" onChange={PriceEvents} value={inputPrice} />
       <button onClick={ListOfItems}> + </button>
       <ol>
         {items.map((itemV,index)=>{
--- practice2/src/list.jsx
+++ practice2/src/list.jsx
@@ -3,8 +3,8 @@ const List =(props)=>{
     
     return(
         <div className="todo_style">
-            <i className="fa fa-times" aria-hidden="true" onClick={()=>{props.onSelect(props.id)}}/>
-            <li>{props.text}</li>
+            <i className="fa fa-times" aria-hidden="true" onClick={()=>{props.onSelect(props.id,props.text.price)}}/>
+            <li>{props.text.item}{props.text.price}</li>
         </div>
         );
 }
