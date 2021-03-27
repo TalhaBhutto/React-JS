50dbcb8bcb893a059d7aaa083a40ee37ee7ae0f1
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-27 19:59:37 +0500

    Update App.js

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -5,6 +5,38 @@ function App() {
   return (
     <div>
       <h1> Welcome to my website</h1>
+      <div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+  </div>
+</div>
     </div>
   );
 }
