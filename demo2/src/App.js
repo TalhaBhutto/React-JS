575b9ab84d52a07609618c4e82322f9483a044bf
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-27 19:42:03 +0500

    Update App.js

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
