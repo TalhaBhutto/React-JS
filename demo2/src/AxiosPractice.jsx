c7b8c004255acafb90a7316b040838e09453ae49
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 17:59:37 +0500

    Update AxiosPractice.jsx

--- demo2/src/AxiosPractice.jsx
+++ demo2/src/AxiosPractice.jsx
@@ -1,11 +1,11 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from "axios";
 
 const Axios=()=>{
     const [num,SetNum]=useState("nothing");
-    // useEffect(()=>{
-        
-    // });   
+     useEffect(()=>{
+        alert("Welcome TO My Website");   
+     });   
 return (<>
 <h1>You have choosen {num}.</h1>
 <select style={{marginBottom:"200px",marginLeft:"30px"}} value={num} onChange={(event)=>{SetNum(event.currentTarget.value)}}>
