15c0fb897e15e73d47bae72702b82901894141cc
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 16:42:39 +0500

    Update Use_Effect_Practice.jsx

--- demo2/src/Use_Effect_Practice.jsx
+++ demo2/src/Use_Effect_Practice.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const =()=>{
+const Effect=()=>{
 
-    return();
+    return(<button onClick={SetNum(num+1)}>Click Me {num}</button>
+    );
 }
 
-export default  ;
+export default  Effect;
