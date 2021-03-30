c82a74015a6d92b9cdde5480d1c979fdace42e9d
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-31 00:04:28 +0500

    Update Menu.jsx

--- demo3/src/Menu.jsx
+++ demo3/src/Menu.jsx
@@ -7,6 +7,7 @@ const Menu=()=>{
     <NavLink exact activeClassName="active_class" to="/"> ABout Us</NavLink>
     <NavLink exact activeClassName="active_class" to="/Contact">Contact</NavLink>
     <NavLink exact activeClassName="active_class" to="/Contact/Twitter">Twitter</NavLink>
+    <NavLink exact activeClassName="active_class" to="/Contact/User">User</NavLink>
     </>);
 }
 export default Menu;
