3f008f483c37617d23106f116e8588bceb297e73
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 23:26:49 +0500

    menu created

--- demo3/src/App.js
+++ demo3/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import {Route, Switch} from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
+import Error from "./Error";
+import Menu from "./Menu";
 
 function App() {
+  const Twitter=()=>{
+    return <h1>Visit at @BhuttoSays</h1>
+  };
   return (
     <>
-
+      
       <Switch>
-        <Route path="/" component={About} />
-        <Route path ="/YehContactPageHay"
+        <Route exact path="/" component={About} />
+        <Route exact path="/YehContactPageHay" component={Contact}/>
+        <Route exact path="/YehContactPageHay/TwitterID" component={Twitter}/>
+        <Route component={Error}/>
       </Switch>
       {/* <About/>
       <Contact/> */}
---
+++ demo3/src/Error.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Error=()=>{
+
+    return(<>
+    <h1>404 Error</h1>
+    </>);
+}
+export default Error;
