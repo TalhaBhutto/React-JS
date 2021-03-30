8b6ac977e3297ce5567cff3ac8f1ee6769829da7
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 16:55:26 +0500

    Update Use_Effect_Practice.jsx

--- demo2/src/Use_Effect_Practice.jsx
+++ demo2/src/Use_Effect_Practice.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 
 const Effect=()=>{
     const [num,SetNum]=useState(0);
-    return(<button onClick={SetNum(num+1)}>Click Me {num}</button>
+    const updteNum=()=>{
+        SetNum(num+1);
+    }
+    return(<button onClick={updteNum}>Click Me {num}</button>
     );
 }
 
