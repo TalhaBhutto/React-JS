ce95207734bc04ef21a9672bb6547635a2d47ba4
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-31 00:43:36 +0500

    useParams

--- demo3/src/Menu.jsx
+++ demo3/src/Menu.jsx
@@ -7,7 +7,7 @@ const Menu=()=>{
     <NavLink exact activeClassName="active_class" to="/"> ABout Us</NavLink>
     <NavLink exact activeClassName="active_class" to="/Contact">Contact</NavLink>
     <NavLink exact activeClassName="active_class" to="/Contact/Twitter">Twitter</NavLink>
-    <NavLink exact activeClassName="active_class" to="/User">User</NavLink>
+    <NavLink exact activeClassName="active_class" to="/User/:name">User</NavLink>
     </>);
 }
 export default Menu;
--- demo3/src/user.jsx
+++ demo3/src/user.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import {useParams} from "react-router-dom";
 
+// const User=({match})=>{
+//     return (<>
+//     <h1>Hello dear {match.params.name} user.</h1>
+//     </>);
+// }
 const User=()=>{
+    const fname=useParams();
     return (<>
-    <h1>Hello dear user.</h1>
+    <h1>Hello dear {fname} user.</h1>
     </>);
 }
 
