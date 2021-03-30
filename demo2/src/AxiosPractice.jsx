d851f00662584243ba419ff4063e40ca2c51f843
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 17:25:27 +0500

    Update AxiosPractice.jsx

--- demo2/src/AxiosPractice.jsx
+++ demo2/src/AxiosPractice.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Axios=()=>{
 return (<>
 <select name="Select Pokemon" id="1" style={{marginBottom:"200px"}}>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
+    <option value="1"></option>
+    <option value="2"></option>
+    <option value="3"></option>
+    <option value="4"></option>
+    <option value="5"></option>
 </select>
 <br/>
 </>);
