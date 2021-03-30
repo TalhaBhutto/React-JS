45f170819e1e9f38fc009613ede60413370b8d14
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 18:15:32 +0500

    Update AxiosPractice.jsx

--- demo2/src/AxiosPractice.jsx
+++ demo2/src/AxiosPractice.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Axios=()=>{
+    let display=false
     const [num,SetNum]=useState("nothing");
     const [name,SetName]=useState();
     const [moves,SetMoves]=useState();
@@ -9,11 +10,16 @@ const Axios=()=>{
      useEffect(()=>{
         async function getD(){
             const res=await axios.get(`https://pokeapi.co/api/v2/pokemo/${num}`);
-        }    
-    });
-    getD();   
+            SetName(res.data.name);
+            SetMoves(res.data.length);
+        }
+        getD();    
+    });   
 return (<>
-<h1>You have choosen {num}.</h1>
+<h1>You have choosen <span style={{color:"red"}}>{num}.</span></h1>
+{
+    if(display)
+}
 <select style={{marginBottom:"200px",marginLeft:"30px"}} value={num} onChange={(event)=>{SetNum(event.currentTarget.value)}}>
     <option value="1">1</option>
     <option value="2">2</option>
