a957c3090c4ced7e46f22494e1f98d8cbff55a45
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 17:02:03 +0500

    Update Use_Effect_Practice.jsx

--- demo2/src/Use_Effect_Practice.jsx
+++ demo2/src/Use_Effect_Practice.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Effect=()=>{
     const [num,SetNum]=useState(0);
-    useEffect(
-        document.title=`Clicked ${num} times`
-    );
+    useEffect(()=>document.title=`Clicked ${num} times`);
     return(<button onClick={()=>SetNum(num+1)}>Click Me {num}</button>
     );
 }
