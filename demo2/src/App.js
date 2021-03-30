04488515fd8753687ec10e77a0974b6d871b17dd
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 16:51:31 +0500

    Update App.js

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import AccessibilityIcon from '@material-ui/icons/Accessibility';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import BS_Cards from "./bootstrap";
 import Effect from "./Use_Effect_Practice";
 
