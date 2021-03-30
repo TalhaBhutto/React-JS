76f2740afb4fdff6e3a9406f4e2ef2f3e318bf04
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 16:43:20 +0500

    Update bootstrap.jsx

--- demo2/src/bootstrap.jsx
+++ demo2/src/bootstrap.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 
 const BS_Cards=()=>{
 
-    return();
+    return(
+        <div>
+      <h1 className="text-center"> Welcome to my website</h1>
+      <div className="container">
+  <div className="row">
+    <div className="col-sm">
+    <div className="card m-2" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/200/150" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+    <div className="card m-2" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/200/150" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col-sm">
+    <div className="card m-2" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/200/150" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+  </div>
+</div>
+    </div>
+    );
 }
 
 export default BS_Cards ;
