f316ecb022c5ddbdb7fc59eb9904db64be403bbe
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-30 17:22:13 +0500

    Use Effect further practice

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import BS_Cards from "./bootstrap";
 import Effect from "./Use_Effect_Practice";
+import Axios from "./AxiosPractice";
 
 function App() {
 
   return (
     <>
+        <Axios></Axios>
         <Effect></Effect>
         <BS_Cards></BS_Cards>
     </>
--- demo2/src/AxiosPractice.jsx
+++ demo2/src/AxiosPractice.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Axios=()=>{
 return (<>
-
+<select name="" id="">1</select>
 </>);
 }
 export default Axios;
