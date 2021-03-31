b14e2d6c6cd949dddcfd8aca193c36286fc82869
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-01 00:38:26 +0500

    Update SearchResult.jsx

--- demo3/src/SearchResult.jsx
+++ demo3/src/SearchResult.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 
-const SearchResult=()=>{
-    const ISource="https://source.unsplash.com/user/erondu/600x400";
+const SearchResult=(Pr)=>{
+    const ISource=`https://source.unsplash.com/user/${pr.name}/600x400`;
   return(<>
   <img src={ISource} style={{margin:"70px 0px 0px 300px"}}/>
   </>);  
