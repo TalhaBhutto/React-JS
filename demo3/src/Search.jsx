e0da52797c6a10fb2db1504fce2aaeda39c6e17d
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-01 00:18:51 +0500

    Update Search.jsx

--- demo3/src/Search.jsx
+++ demo3/src/Search.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {useParams,useLocation,useHistory} from "react-router-dom";
 
 
 const Search=()=>{
+    const [image,NewImage]=useState();
   return(<>
-  
+  <div style={{margin:"20px 100px 20px 0px"}}>
+  <input type="text" onChange={} placeholder="Search ANything"></input>
+  </div>
   </>);  
 }
 
