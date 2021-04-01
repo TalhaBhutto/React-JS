ae7bc66623fcf6249e57e9f05a1334e9cad37613
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-01 23:50:20 +0500

    Update user.jsx

--- demo3/src/user.jsx
+++ demo3/src/user.jsx
@@ -11,7 +11,7 @@ const User=()=>{
     const {fname,lname}=useParams();
     const location = useLocation();
     return (<>
-    <h1>Hello dear {fname} {lname}. Wellcome</h1>
+    <h1>Hello dear {fname} {lname}. Wellcome to</h1>
     <p>My current location is {location.pathname}</p>
     {
         location.pathname===`/User/Talha/Hussain`?<button onClick={()=>history.push("/")}>Back</button>:null
